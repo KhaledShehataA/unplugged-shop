85482facc04aa46001328a5ef2d8494e3d318a0a
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-07-01 16:47:33 +0200

    feat: Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
 	return (
@@ -13,7 +14,12 @@ const Product = ({ product }) => {
 						<strong>{product.name}</strong>
 					</Card.Title>
 				</a>
-				<Card.Text as='div'></Card.Text>
+				<Card.Text as='div'>
+					<Rating
+						value={product.rating}
+						text={`${product.numReviews} reviews`}
+					/>
+				</Card.Text>
 				<Card.Text as='h3'>${product.price}</Card.Text>
 			</Card.Body>
 		</Card>
