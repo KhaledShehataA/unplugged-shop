4bf2d95d6c775e34229fd8aaa205765bf753136c
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-07-07 06:39:27 +0200

    backend: modeling our data

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import Mongoose from 'mongoose';
+
+const userSchema = Mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
