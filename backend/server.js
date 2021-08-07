6b5ba6600911af790a9c91792b69e3ffeef849c7
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-08-08 01:40:01 +0200

    WIP: prepare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,20 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running...');
+	});
+}
 
 app.use('/api/products', productRoutes);
 
